66f7e85bf28e606909216ff74f2260114a2c3f7b
jasonWuTw <jason.wu.tw@gmail.com>
2023-10-26 14:31:47 +0800

    button & while loop

--- button.c
+++ button.c
@@ -2,23 +2,23 @@
 #include "nrf_delay.h"
 #include "nrf_log.h"
 
-#define BUTTON_PIN NRF_GPIO_PIN_MAP(0,4)
-#define LED_PIN NRF_GPIO_PIN_MAP(0,6)
+#define MODE_UP NRF_GPIO_PIN_MAP(0,4)
+#define RM_LED1 NRF_GPIO_PIN_MAP(0,6)
 
 void gpio_init() {
-    nrf_gpio_cfg_input(BUTTON_PIN, NRF_GPIO_PIN_PULLUP);
-    nrf_gpio_cfg_output(LED_PIN);
-    nrf_gpio_pin_set(LED_PIN); // Turn the LED off by default
+    nrf_gpio_cfg_input(MODE_UP, NRF_GPIO_PIN_PULLUP);
+    nrf_gpio_cfg_output(RM_LED1);
+    nrf_gpio_pin_set(RM_LED1); // Turn the LED off by default
 }
 
 int main(void) {
 	NRF_LOG_INFO("main...");
     gpio_init();
     while (1) {
-        if (!nrf_gpio_pin_read(BUTTON_PIN)) { // Button is active low
-            nrf_gpio_pin_clear(LED_PIN); // Turn on LED
+        if (!nrf_gpio_pin_read(MODE_UP)) { // Button is active low
+            nrf_gpio_pin_clear(RM_LED1); // Turn on LED
             nrf_delay_ms(500); // Delay for 500ms
-            nrf_gpio_pin_set(LED_PIN); // Turn off LED
+            nrf_gpio_pin_set(RM_LED1); // Turn off LED
             nrf_delay_ms(500); // Delay for 500ms
 			//NRF_LOG_INFO("button click...");
         }

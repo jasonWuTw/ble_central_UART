a70e55b8a3b50bcb36a597727e8f7781de27d49d
jasonWuTw <jason.wu.tw@gmail.com>
2023-10-27 17:02:27 +0800

    NRF_ERROR_NO_MEM

--- ble_peripheral/main.c
+++ ble_peripheral/main.c
@@ -728,10 +728,10 @@ static void buttons_leds_init(bool * p_erase_bonds)
 
     //不註解,出現[NRF_ERROR_NO_MEM]
     //註解,NRF_LOG_INFO出不來
-    //uint32_t err_code = bsp_init(BSP_INIT_LEDS | BSP_INIT_BUTTONS, bsp_event_handler);
-    //APP_ERROR_CHECK(err_code);
+    uint32_t err_code = bsp_init(NULL, bsp_event_handler);
+    APP_ERROR_CHECK(err_code);
 
-    uint32_t err_code = bsp_btn_ble_init(NULL, &startup_event);
+    err_code = bsp_btn_ble_init(NULL, &startup_event);
     APP_ERROR_CHECK(err_code);
 
     *p_erase_bonds = (startup_event == BSP_EVENT_CLEAR_BONDING_DATA);

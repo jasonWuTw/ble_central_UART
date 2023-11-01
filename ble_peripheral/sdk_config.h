d4057e00aedaf4f758a85d9a01979c99b9287aa0
jasonWuTw <jason.wu.tw@gmail.com>
2023-11-01 18:15:04 +0800

    TIMER1_ENABLED 1

--- ble_peripheral/sdk_config.h
+++ ble_peripheral/sdk_config.h
@@ -3892,7 +3892,7 @@
  
 
 #ifndef NRFX_TIMER0_ENABLED
-#define NRFX_TIMER0_ENABLED 0
+#define NRFX_TIMER0_ENABLED 1
 #endif
 
 // <q> NRFX_TIMER1_ENABLED  - Enable TIMER1 instance
@@ -5664,7 +5664,7 @@
  
 
 #ifndef TIMER1_ENABLED
-#define TIMER1_ENABLED 0
+#define TIMER1_ENABLED 1
 #endif
 
 // <q> TIMER2_ENABLED  - Enable TIMER2 instance

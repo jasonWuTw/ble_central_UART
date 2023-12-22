b4985940ee66f037107faec9410c25c76089904c
jasonWuTw <jason.wu.tw@gmail.com>
2023-12-22 14:21:10 +0800

    改為Matsutek_E-Bike

--- ble_peripheral/main.c
+++ ble_peripheral/main.c
@@ -49,7 +49,7 @@
 
 #define APP_BLE_CONN_CFG_TAG            1                                           /**< A tag identifying the SoftDevice BLE configuration. */
 
-#define DEVICE_NAME                     "Matsutek_E-Bike-wh"                               /**< Name of device. Will be included in the advertising data. */
+#define DEVICE_NAME                     "Matsutek_E-Bike"                               /**< Name of device. Will be included in the advertising data. */
 #define NUS_SERVICE_UUID_TYPE           BLE_UUID_TYPE_VENDOR_BEGIN                  /**< UUID type for the Nordic UART Service (vendor specific). */
 
 #define APP_BLE_OBSERVER_PRIO           3                                           /**< Application's BLE observer priority. You shouldn't need to modify this value. */
